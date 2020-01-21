6105035db4f9e1e1eefd813f1a84b95c0501914f
DanteAkito <dante_s2040@hotmail.com>
2020-01-21 09:29:40 -0500

    variables

--- basics.py
+++ basics.py
@@ -1,2 +1,18 @@
 import datetime
-print(datetime.datetime.now())
+mynow = datetime.datetime.now()
+print("The date and time is :", datetime.datetime.now())
+print(mynow)
+mynumber = 31
+mytext = "fuck"
+print(mynumber, mytext) 
+x = 10
+y = "10"
+
+sum1 = x + x
+sum2 = y + y
+print(sum1, sum2)
+
+
+
+
+#Uso de variables. el signo (-) menos es reservado como operador de python
